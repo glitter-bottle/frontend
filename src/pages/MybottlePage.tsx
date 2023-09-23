a9059ccb8c931725a1afd3aef38cbb3721bf50af
yuseon <dbtjs3@naver.com>
2023-09-23 21:01:36 +0900

    Feat: mybottle 기능 추가

--- src/pages/MybottlePage.tsx
+++ src/pages/MybottlePage.tsx
@@ -1,12 +1,33 @@
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from '../components/Header';
+import MybottleSection from '../features/MyBottlePage';
 
 const MybottlePage = () => {
+  const location = useLocation();
+  const nickName = localStorage.getItem('nickName');
+
+  if(nickName === null) {
+    alert('💁‍♀️ 로그인 후 이용해주세요');
+    window.location.href = '/';
+  }
   return (
     <div>
       <Header />
+      <Title>내 보틀에 담은 문장이에요</Title>
+      <MybottleSection />
     </div>
   )
 }
+
+
+
+
+const Title = styled.h2`
+  font-size: 1.6rem;
+  text-align: center;
+  margin: 2rem 0;
+  font-weight: 500;
+`
 
 export default MybottlePage
